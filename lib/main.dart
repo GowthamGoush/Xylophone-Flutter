c8e6f382a24f09081dc360e6651790ea4afd4a5e
Rangaraj-baskar <72215163+Rangaraj-baskar@users.noreply.github.com>
2022-10-25 23:33:39 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; // imports the package material.dart
 import 'package:audioplayers/audioplayers.dart';
 
 void main() {
